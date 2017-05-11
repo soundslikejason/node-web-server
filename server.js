bedf678b2456aba00d80b4c0bf371c2d16b73b6b
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-11 11:09:21 -0700

    send json

--- server.js
+++ server.js
@@ -3,7 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express</h1>');
+  // res.send('<h1>Hello Express</h1>');
+  res.send({
+    name: 'Jason',
+    likes: [
+      'Music',
+      'Cities'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error handling request'
+  });
 });
 
 app.listen(3000);
